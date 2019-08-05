275f4776acfcc65156dd081d96482030ce24c15e
Daniel Wilson <daniel@loylap.com>
2019-08-05 13:06:55 +0100

    Simple Order manipulation attempt for order-less Loyalty Payments

--- sampleloyaltyapp/src/main/java/com/sampleloyaltyapp/MainActivity.java
+++ sampleloyaltyapp/src/main/java/com/sampleloyaltyapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.sampleloyaltyapp;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +15,18 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import co.poynt.api.model.Discount;
+import co.poynt.api.model.Order;
+import co.poynt.api.model.OrderAmounts;
 import co.poynt.api.model.OrderStatus;
+import co.poynt.api.model.OrderStatuses;
 import co.poynt.os.model.Intents;
 import co.poynt.os.model.Payment;
+import co.poynt.os.model.PoyntError;
+import co.poynt.os.services.v1.IPoyntOrderService;
+import co.poynt.os.services.v1.IPoyntOrderServiceListener;
 
 public class MainActivity extends Activity {
 
@@ -31,35 +43,68 @@ public class MainActivity extends Activity {
         addDiscount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Order paymentOrder = payment.getOrder();
+                if(paymentOrder == null){
+                    //Handle Terminal only payments, which do not possess an Order
+                    paymentOrder = new Order();
+                    paymentOrder.setId(UUID.randomUUID());
+                    payment.setOrder(paymentOrder);
+                }
+
                 long discountAmount = 100l;
                 List<Discount> discounts = payment.getOrder().getDiscounts();
                 if (discounts == null) {
                     discounts = new ArrayList<>();
                 }
                 Discount discount = new Discount();
-                // add one dollar discount at order level
-                // NOTE: discount amount is always negative
                 discount.setAmount(-1l * discountAmount);
                 discount.setCustomName("Loyalty Discount");
                 discount.setProcessor(getPackageName());
                 discounts.add(discount);
                 payment.getOrder().setDiscounts(discounts);
-                // discount total should be updated
-                long discountTotal = payment.getOrder().getAmounts().getDiscountTotal() + discount.getAmount();
-                payment.getOrder().getAmounts().setDiscountTotal(discountTotal);
-                // update the over all total
-                long orderTotal = payment.getOrder().getAmounts().getNetTotal();
+
+                //long discountTotal = payment.getOrder().getAmounts().getDiscountTotal() + discount.getAmount();
+                //payment.getOrder().getAmounts().setDiscountTotal(discountTotal);
+
+                OrderAmounts amounts = new OrderAmounts();
+                amounts.setDiscountTotal(-1l * discountAmount);
+                amounts.setSubTotal(payment.getAmount());
+                payment.getOrder().setAmounts(amounts);
+
+                long orderTotal = payment.getOrder().getAmounts().getSubTotal();
                 if (orderTotal >= discountAmount) {
                     payment.getOrder().getAmounts().setNetTotal(orderTotal - discountAmount);
                     payment.setAmount(orderTotal - discountAmount);
-                    if (orderTotal == discountAmount){
-                        payment.getOrder().getStatuses().setStatus(OrderStatus.COMPLETED);
+
+                    if(payment.getOrder().getStatuses() == null){
+                        payment.getOrder().setStatuses(new OrderStatuses());
                     }
-                    Log.d(TAG, "Discount added to order: " + payment);
-                    Intent result = new Intent(Intents.ACTION_PROCESS_LOYALTY_RESULT);
-                    result.putExtra("payment", payment);
-                    setResult(Activity.RESULT_OK, result);
-                    finish();
+                    payment.getOrder().getStatuses().setStatus(OrderStatus.COMPLETED);
+
+                    try {
+                        poyntOrderService.createOrder(payment.getOrder(), UUID.randomUUID().toString(), new IPoyntOrderServiceListener() {
+                            @Override
+                            public void orderResponse(Order order, String s, PoyntError poyntError) throws RemoteException {
+
+                                Log.d(TAG, "Discount added to order: " + payment);
+                                Intent result = new Intent(Intents.ACTION_PROCESS_LOYALTY_RESULT);
+                                result.putExtra("payment", payment);
+                                setResult(Activity.RESULT_OK, result);
+                                finish();
+                            }
+
+                            @Override
+                            public IBinder asBinder() {
+                                Intent result = new Intent(Intents.ACTION_PROCESS_LOYALTY_RESULT);
+                                result.putExtra("payment", payment);
+                                setResult(Activity.RESULT_OK, result);
+                                finish();
+                                return null;
+                            }
+                        });
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
                 }else{
                     Toast.makeText(MainActivity.this, "Discount amount is larger than total", Toast.LENGTH_SHORT).show();
                     setResult(Activity.RESULT_CANCELED);
@@ -83,6 +128,33 @@ public class MainActivity extends Activity {
         // to check in.
     }
 
+    private IPoyntOrderService poyntOrderService = null;
+    private ServiceConnection orderConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            poyntOrderService = IPoyntOrderService.Stub.asInterface(iBinder);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            poyntOrderService = null;
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        bindService(Intents.getComponentIntent(Intents.COMPONENT_POYNT_ORDER_SERVICE), orderConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unbindService(orderConnection);
+    }
+
     private void handleIntent(Intent intent) {
         String action = intent.getAction();
 
@@ -96,10 +168,10 @@ public class MainActivity extends Activity {
             } else {
                 // add a discount to the order
                 if (payment.getOrder() == null) {
-                    Log.e(TAG, "launched with no order object");
+                    /*Log.e(TAG, "launched with no order object");
                     Intent result = new Intent(Intents.ACTION_PROCESS_LOYALTY_RESULT);
                     setResult(Activity.RESULT_CANCELED, result);
-                    finish();
+                    finish();*/
                 } else {
                     // wait till the Add discount is clicked
                 }
--- sampleloyaltyapp/src/main/java/com/sampleloyaltyapp/SampleLoyaltyService.java
+++ sampleloyaltyapp/src/main/java/com/sampleloyaltyapp/SampleLoyaltyService.java
@@ -32,50 +32,11 @@ public class SampleLoyaltyService extends Service {
                             IPoyntLoyaltyServiceListener iPoyntLoyaltyServiceListener)
                 throws RemoteException {
             Log.d(TAG, "process(): " + payment);
-            // add a discount to the order
-            Order order = payment.getOrder();
-            if (order != null) {
 
-
-/*
-                // uncomment this block if you want to apply discount without launching an activity
-                // to collect additional input
-
-                List<Discount> discounts = order.getDiscounts();
-                if (discounts == null) {
-                    discounts = new ArrayList<>();
-                }
-                Discount discount = new Discount();
-                // add one dollar discount at order level
-                // NOTE: discount amount is always negative
-                discount.setAmount(-100l);
-                discount.setCustomName("Loyalty Discount");
-                discount.setProcessor(getPackageName());
-                discounts.add(discount);
-                order.setDiscounts(discounts);
-                // discount total should be updated
-                long discountTotal = order.getAmounts().getDiscountTotal() + discount.getAmount();
-                order.getAmounts().setDiscountTotal(discountTotal);
-                // update the over all total
-                long orderTotal = order.getAmounts().getNetTotal();
-                order.getAmounts().setNetTotal(orderTotal - 100l);
-                payment.setAmount(orderTotal - 100l);
-                Log.d(TAG, "Discount added to order: " + payment);
-                iPoyntLoyaltyServiceListener.loyaltyApplied(payment, requestId);
-
-*/
-
-                // if you want to collect additional info before processing
-                Intent intent = new Intent(Intents.ACTION_PROCESS_LOYALTY);
-                intent.setComponent(new ComponentName(getPackageName(), MainActivity.class.getName()));
-                intent.putExtra("payment", payment);
-                iPoyntLoyaltyServiceListener.onLaunchActivity(intent, requestId);
-            } else {
-                Log.d(TAG, "No Discount added - order is required to add discounts");
-                iPoyntLoyaltyServiceListener.noLoyaltyApplied(requestId);
-            }
-
-
+            Intent intent = new Intent(Intents.ACTION_PROCESS_LOYALTY);
+            intent.setComponent(new ComponentName(getPackageName(), MainActivity.class.getName()));
+            intent.putExtra("payment", payment);
+            iPoyntLoyaltyServiceListener.onLaunchActivity(intent, requestId);
         }
     };
 }
